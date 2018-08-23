1d443aef6f710a4fabd30fbc37fb2e1b982ff768
Дмитрий Селин <dvs@anadeainc.com>
2018-08-23 15:16:28 +0300

    fixes and debug logs

--- app/process-player.js
+++ app/process-player.js
@@ -16,42 +16,38 @@ function processPlayerMove (roversActions, player, fieldData, players) {
     if (rover) {
       switch (action.action_type) {
         case 'move':
-          response.errors[rover.id] = [];
-          action.moves.slice(0, ROVER.MAX_MOVES).forEach((move, index) => {
-            // check if this move is possible;
-            if (rover.energy <= 0) {
-              response.errors[rover.id][index] = {code: ERRORS.NO_ENERGY, message: 'lack of energy, please charge'};
-            } else if (![-1, 0, 1].includes(move.dx) ||
-            ![-1, 0, 1].includes(move.dy)) {
-              response.errors[rover.id][index] = {code: ERRORS.WRONG_MOVE, message: 'wrong move'};
+          // check if this move is possible;
+          if (rover.energy <= 0) {
+            response.errors[rover.id] = {code: ERRORS.NO_ENERGY, message: 'lack of energy, please charge'};
+          } else if (![-1, 0, 1].includes(action.dx) ||
+          ![-1, 0, 1].includes(action.dy)) {
+            response.errors[rover.id] = {code: ERRORS.WRONG_MOVE, message: 'wrong move'};
+          } else {
+            const newX = (rover.x + action.dx + fieldData.FIELD_SIZE) % fieldData.FIELD_SIZE; // make field round connected
+            const newY = (rover.y + action.dy + fieldData.FIELD_SIZE) % fieldData.FIELD_SIZE;
+
+            if (fieldData.field[newY][newX] === TERRAIN.BASE && (newX !== player.base.x || newY !== player.base.y)) {
+              response.errors[rover.id] = {code: ERRORS.NO_MOVE_TO_FOREIGN_BASE, message: 'cannot move to foreign base'};
+            } else if (players.some(otherplayer =>
+              otherplayer.rovers.some(otherrover => // including also other rovers of same player
+                otherrover.x === newX && otherrover.y === newY
+              ))) {
+              response.errors[rover.id] = {code: ERRORS.NO_MOVE_TO_FOREIGN_ROVER, message: 'cannot move, tile occupied by other rover'};
             } else {
-              const newX = rover.x + move.dx;
-              const newY = rover.y + move.dy;
-              if (newX < 0 || newX >= fieldData.FIELD_SIZE ||
-                newY < 0 || newY >= fieldData.FIELD_SIZE) {
-                response.errors[rover.id][index] = {code: ERRORS.OUT_OF_BOUND, message: 'move out of bound'};
-              } else if (fieldData.field[newY][newX] === TERRAIN.BASE && (rover.x !== player.base.x || rover.y !== player.base.y)) {
-                response.errors[rover.id][index] = {code: ERRORS.NO_MOVE_TO_FOREIGN_BASE, message: 'cannot move to foreign base'};
-              } else if (players.some(otherplayer =>
-                otherplayer.rovers.some(otherrover => // including also other rovers of same player
-                  otherrover.x === newX && otherrover.y === newY
-                ))) {
-                response.errors[rover.id][index] = {code: ERRORS.NO_MOVE_TO_FOREIGN_ROVER, message: 'cannot move, tile occupied by other rover'};
-              } else {
-                rover.x = newX;
-                rover.y = newY;
-                rover.energy -= 1;
-                if (rover.x === player.base.x && rover.y === player.base.y) { // rover came to own base
-                  rover.energy = ROVER.MAX_ENERGY;
-                  rover.load.forEach(resource => {
-                    player.resources[resource] += 1;
-                  });
-                  rover.load = [];
-                }
-                console.log('processed rover move', newX, newY);
+              rover.x = newX;
+              rover.y = newY;
+              rover.energy -= 1;
+              if (rover.x === player.base.x && rover.y === player.base.y) { // rover came to own base
+                rover.energy = ROVER.MAX_ENERGY;
+                rover.load.forEach(resource => {
+                  player.resources[resource] += 1;
+                });
+                rover.load = [];
               }
+              console.log('processed rover move', newX, newY);
             }
-          });
+          }
+
           break;
         case 'dig':
           if (rover.x === player.base.x && rover.y === player.base.y) {
@@ -68,10 +64,10 @@ function processPlayerMove (roversActions, player, fieldData, players) {
             rover.load.push(resource);
             player.points += resource2points[resource];
             fieldData.resources[rover.y][rover.x] = RESOURCES.HOLE; // there was already dug here
-            console.log('dig at ', rover.x, rover.y);
+            console.log('dig at ', rover.x, rover.y, 'got', rover.load);
           } else {
             fieldData.resources[rover.y][rover.x] = RESOURCES.HOLE; // there was already dug here
-            console.log('dig at ', rover.x, rover.y);
+            console.log('dig empty at ', rover.x, rover.y);
           }
           break;
         case 'charge':
@@ -79,29 +75,25 @@ function processPlayerMove (roversActions, player, fieldData, players) {
           break;
       }
 
-      // TODO prepare info about new state of rover and its surroundings (may be other player dig smth)
+      // prepare info about new state of rover and its surroundings (may be other player dig smth)
       const area = [[], [], []];
       for (let y = 0; y < 3; y++) {
         for (let x = 0; x < 3; x++) {
-          const fieldX = rover.x + x - 1;
-          const fieldY = rover.y + y - 1;
-          if (fieldX >= 0 && fieldX < fieldData.FIELD_SIZE &&
-            fieldY >= 0 && fieldY < fieldData.FIELD_SIZE) {
-            area[y][x] = {
-              terrain: fieldData.field[fieldY][fieldX],
-              objects: fieldData.resources[fieldY][fieldX] === RESOURCES.HOLE ? [OBJECTS.HOLE] : []
+          const fieldX = (rover.x + x - 1 + fieldData.FIELD_SIZE) % fieldData.FIELD_SIZE;
+          const fieldY = (rover.y + y - 1 + fieldData.FIELD_SIZE) % fieldData.FIELD_SIZE;
+
+          area[y][x] = {
+            terrain: fieldData.field[fieldY][fieldX],
+            objects: fieldData.resources[fieldY][fieldX] === RESOURCES.HOLE ? [OBJECTS.HOLE] : []
+          };
+          players.forEach(otherplayer => {
+            if (otherplayer.rovers.some(otherrover => fieldX === otherrover.x && fieldY === otherrover.y)) {
+              area[y][x].objects.push(OBJECTS.ROVER);
             };
-            players.forEach(otherplayer => {
-              if (otherplayer.rovers.some(otherrover => fieldX === otherrover.x && fieldY === otherrover.y)) {
-                area[y][x].objects.push(OBJECTS.ROVER);
-              };
-              if (fieldX === otherplayer.base.x && fieldY === otherplayer.base.y) {
-                area[y][x].objects.push(OBJECTS.BASE);
-              }
-            });
-          } else {
-            area[y][x] = {};
-          }
+            if (fieldX === otherplayer.base.x && fieldY === otherplayer.base.y) {
+              area[y][x].objects.push(OBJECTS.BASE);
+            }
+          });
         }
       }
 
--- client/controllers/move-controller.js
+++ client/controllers/move-controller.js
@@ -1,9 +1,13 @@
 let digging = true;
 const ROVER_ID = process.env.ROVER_ID || 1;
 const MAX_ATTEMPTS = 10;
+const MAX_LOAD = 3;
+
+const base = {};
+
 module.exports = (req, res, next) => {
   const errors = req.body.errors;
-  if (errors) {
+  if (Object.keys(errors).length > 0) {
     console.log('errors', errors);
   }
   const rover = req.body.rovers ? req.body.rovers[0] : null;
@@ -11,29 +15,44 @@ module.exports = (req, res, next) => {
     console.log('wrong rover', req.body.rovers, req.body);
     res.json({error: 'no rovers data'});
   } else {
-    digging = !digging;
-    let randomX = 0;
-    let randomY = 0;
-    let attempt = 0;
-    let cellDoesntExist = false;
-    let cellNotEmpty = false;
-    do {
-      let randomMove = Math.floor(Math.random() * 8);
-      if (randomMove > 3) randomMove++; // we want 0,1,2,3 5,6,7,8
-      randomX = Math.floor(randomMove / 3);
-      randomY = randomMove % 3;
-      attempt++;
-      cellDoesntExist = rover.area && rover.area[randomY][randomX] && (typeof rover.area[randomY][randomX].terrain === 'undefined'); // area[y][x] === {} for non existant cells
-      cellNotEmpty = rover.area &&
-        rover.area[randomY][randomX] &&
-        rover.area[randomY][randomX].objects &&
-        rover.area[randomY][randomX].objects.length !== 0;
-    } while (cellDoesntExist || (cellNotEmpty && attempt < MAX_ATTEMPTS)); // go to buzy cells if no viable move was found during 10 attempts
-    console.log(digging ? `move dx=${randomX - 1}, dy=${randomY - 1}` : 'dig');
+    if (typeof base.x === 'undefined' && typeof base.y === 'undefined') {
+      base.x = rover.x;
+      base.y = rover.y;
+    }
+    if (rover.energy > 0) {
+      if (rover.load && rover.load.length >= MAX_LOAD) { // moving home
+        const dx = rover.x < base.x ? 1 : rover.x > base.x ? -1 : 0;
+        const dy = rover.y < base.y ? 1 : rover.y > base.y ? -1 : 0;
+        res.json([{rover_id: ROVER_ID, action_type: 'move', dx, dy}]);
+      } else {
+        digging = !digging;
+        if (digging) {
+          res.json([{ rover_id: ROVER_ID, action_type: 'dig' }]);
+        } else {
+          let randomX = 0;
+          let randomY = 0;
+          let attempt = 0;
 
-    res.json([ digging
-      ? {rover_id: ROVER_ID, action_type: 'move', moves: [{dx: randomX - 1, dy: randomY - 1}]}
-      : {rover_id: ROVER_ID, action_type: 'dig'}
-    ]);
+          let cellNotEmpty = false;
+          do {
+            let randomMove = Math.floor(Math.random() * 8);
+            if (randomMove > 3) randomMove++; // we want 0,1,2,3 5,6,7,8
+            randomX = Math.floor(randomMove / 3);
+            randomY = randomMove % 3;
+            attempt++;
+
+            cellNotEmpty = rover.area &&
+              rover.area[randomY][randomX] &&
+              rover.area[randomY][randomX].objects &&
+              rover.area[randomY][randomX].objects.length !== 0;
+          } while (cellNotEmpty && attempt < MAX_ATTEMPTS); // go to buzy cells if no viable move was found during 10 attempts
+          console.log(digging ? `move dx=${randomX - 1}, dy=${randomY - 1}` : 'dig');
+
+          res.json([{rover_id: ROVER_ID, action_type: 'move', dx: randomX - 1, dy: randomY - 1}]);
+        }
+      }
+    } else {
+      res.json([{rover_id: ROVER_ID, action_type: 'charge'}]);
+    }
   }
 };
